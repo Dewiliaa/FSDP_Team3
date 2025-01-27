eeff7051dff1b2492c1fb07dabf1b2be8a9dd628
Doooddle <S10257347@connect.np.edu.sg>
2025-01-27 14:35:35 +0800

    added comment

--- fsdp/src/config.js
+++ fsdp/src/config.js
@@ -1,6 +1,6 @@
 const config = {
     apiBaseUrl: process.env.REACT_APP_API_BASE_URL || 'http://192.168.86.28:3000',
-    wsUrl: process.env.REACT_APP_WS_URL || 'http://192.168.86.28:3001'  // <-- WebSocket URL
+    wsUrl: process.env.REACT_APP_WS_URL || 'http://192.168.86.28:3001'  // Change to your server's IP address
 };
 
 export default config;
