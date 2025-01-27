d368275ca7b43fbbf6826ee13555defd4e006342
Doooddle <S10257347@connect.np.edu.sg>
2025-01-27 15:37:09 +0800

    added production env to test hosting

--- fsdp/src/App.js
+++ fsdp/src/App.js
@@ -27,7 +27,7 @@ const ProtectedRoute = ({ children }) => {
         // Validate token with backend
         const validateToken = async () => {
             try {
-                const response = await fetch(config.wsUrl +'/api/auth/validate', {
+                const response = await fetch(config.apiBaseUrl +'/api/auth/validate', {
                     headers: {
                         'Authorization': `Bearer ${token}`
                     }
