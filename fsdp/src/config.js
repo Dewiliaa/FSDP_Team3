bfa9f2cbcef688904bf3f0ca479f7027c7293a96
Doooddle <S10257347@connect.np.edu.sg>
2025-02-01 11:29:26 +0800

    changed config ip to match elastic ip

--- fsdp/src/config.js
+++ fsdp/src/config.js
@@ -1,6 +1,6 @@
 const config = {
-    apiBaseUrl: process.env.REACT_APP_API_BASE_URL || 'http://ec2-54-252-192-167.ap-southeast-2.compute.amazonaws.com:3001',  // For API calls
-    socketUrl: process.env.REACT_APP_SOCKET_URL || 'http://ec2-54-252-192-167.ap-southeast-2.compute.amazonaws.com:3001'      // For socket.io
+    apiBaseUrl: process.env.REACT_APP_API_BASE_URL || 'http://52.65.65.69:3001/',  // For API calls
+    socketUrl: process.env.REACT_APP_SOCKET_URL || 'http://52.65.65.69:3001/'      // For socket.io
 };
 //change both
 export default config;
