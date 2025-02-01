2a672808fa4bde101f4352efa7e3dcdc2465c9f9
Doooddle <S10257347@connect.np.edu.sg>
2025-02-01 11:01:46 +0800

    link changes

--- fsdp/src/config.js
+++ fsdp/src/config.js
@@ -1,6 +1,6 @@
 const config = {
-    apiBaseUrl: process.env.REACT_APP_API_BASE_URL || 'http://192.168.1.97:3001',  // For API calls
-    socketUrl: process.env.REACT_APP_SOCKET_URL || 'http://192.168.1.97:3001'      // For socket.io
+    apiBaseUrl: process.env.REACT_APP_API_BASE_URL || 'http://ec2-54-252-192-167.ap-southeast-2.compute.amazonaws.com:3001',  // For API calls
+    socketUrl: process.env.REACT_APP_SOCKET_URL || 'http://ec2-54-252-192-167.ap-southeast-2.compute.amazonaws.com:3001'      // For socket.io
 };
 //change both
 export default config;
